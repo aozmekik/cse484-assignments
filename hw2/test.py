026989641cb1a9f9c55b1dbadf47e17e4654941b
Ahmed Semih Özmekik <ahmed-semih@outlook.com>
2020-11-28 13:23:56 +0300

    language models done

--- hw2/test.py
+++ hw2/test.py
@@ -1,5 +1,7 @@
-from nltk import ngrams
 import random
+import re
+from nltk import ngrams, trigrams
+from collections import Counter, defaultdict
 
 
 def read_file(file):
@@ -11,6 +13,11 @@ def read_file(file):
     return words
 
 
+def normalize_data(data):
+    # remove everything except the letters.
+    return re.sub(r'[^A-Za-z. ]', '', data.lower())
+
+
 def get_sample(file, sample_name, N=20):
     '''
         Gets N sample randomly from given word list and write to the file.
@@ -19,22 +26,43 @@ def get_sample(file, sample_name, N=20):
         @param N, sample count.
     '''
     words = read_file(file)
+    print(int(len(words) * (N/100)))
     words = ' '.join([words[i] for i in range(int(len(words) * (N/100)))])
     # words = ' '.join([word for word in words])
     with open(sample_name, 'w', encoding='utf-8') as file:
         file.write(words)
 
-def 
+
+def model(words, n):
+    if n == 1:
+        # count
+        counter = Counter(ngrams(words, 1))
+        M = {word[0]: counter[word] for word in counter}
+
+        # counts to probabilities
+        total_count = float(sum(M.values()))
+        for w in M:
+            M[w] /= total_count
+    else:
+        # count
+        M = defaultdict(lambda: defaultdict(lambda: 0))
+        for w in ngrams(words, n, pad_right=True, pad_left=True):
+            M[w[: n-1]][w[-1]] += 1
+
+        # counts to probabilities
+        for w1_w2 in M:
+            total_count = float(sum(M[w1_w2].values()))
+            for w3 in M[w1_w2]:
+                M[w1_w2][w3] /= total_count
+
+    return M
 
 
-get_sample('data/trwiki', 'sample0.txt')
+def build_models(words):
+    models = []
+    for n in range(1, 6):
+        models.append(model(words, n))
 
 
-
-# words = read_file('data/trwiki')
-
-# n = 6
-# sixgrams = ngrams(words, n)
-
-# for grams in sixgrams:
-#   print(grams)
+words = read_file('sample0.txt')
+models = build_models(words)
