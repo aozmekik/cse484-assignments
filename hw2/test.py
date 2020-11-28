fe1c2585d0266135418b6cc9d066f16721ebb46a
Ahmed Semih Özmekik <ahmed-semih@outlook.com>
2020-11-28 20:18:12 +0300

    perplexity and gt-smoothing.

--- hw2/test.py
+++ hw2/test.py
@@ -3,19 +3,27 @@ import re
 from nltk import ngrams, trigrams
 from collections import Counter, defaultdict
 
+# TASKS.
+# TODO. gt-smoothing.
+# TODO. produce random sentences.
+#
+
 
 def read_file(file):
-    words = []
+    X = []
     with open(file, 'r', encoding='utf-8', errors='ignore') as file:
-        # words = file.read().strip().split(' ')
-        words = file.read()
-        words = words.strip().split()
-    return words
+        X = normalize(file.read()).split()
+    return X
 
 
-def normalize_data(data):
-    # remove everything except the letters.
-    return re.sub(r'[^A-Za-z. ]', '', data.lower())
+def normalize(X):
+    # just lower case letters.
+    return re.sub(r'[^A-Za-z. ]', '', X.lower().strip())
+
+
+def split(X, ratio=0.05):
+    N = int(len(X) * (1-ratio))
+    return X[:N], X[N:]
 
 
 def get_sample(file, sample_name, N=20):
@@ -25,12 +33,49 @@ def get_sample(file, sample_name, N=20):
         @param sample_name, name of the new file containing N samples.
         @param N, sample count.
     '''
-    words = read_file(file)
-    print(int(len(words) * (N/100)))
-    words = ' '.join([words[i] for i in range(int(len(words) * (N/100)))])
+    X = read_file(file)
+    print(int(len(X) * (N/100)))
+    X = ' '.join([X[i] for i in range(int(len(X) * (N/100)))])
     # words = ' '.join([word for word in words])
     with open(sample_name, 'w', encoding='utf-8') as file:
-        file.write(words)
+        file.write(X)
+
+
+def smooth(M, words, n):
+    Nc = defaultdict(lambda: 0)
+    N = 0
+    if n == 1:
+        # count all the counts
+        for w in M:
+            c = M[w]
+            Nc[c] += 1
+            N += c
+
+        # unigrams that never occured
+        Nc[0] = len(words) - N
+
+        for w in M:
+            c = M[w]
+            M[w] = (c + 1) * (N[c+1]/N[c])
+    else:
+        # count all the counts
+        for wi in M:
+            for wj in M[wi]:
+                c = M[wi][wj]
+                Nc[c] += 1
+                N += c
+
+        # ngrams that never occured
+        Nc[0] = len(words)**n - N
+
+        # apply good turing smoothing
+        for wi in M:
+            for wj in M[wi]:
+                c = M[wi][wj]
+                M[wi][wj] = (c + 1) * (N[c+1]/N[c])
+
+    # revised count for ngrams that never occured
+    M[None] = N[1] / N[0]
 
 
 def model(words, n):
@@ -38,6 +83,9 @@ def model(words, n):
         # count
         counter = Counter(ngrams(words, 1))
         M = {word[0]: counter[word] for word in counter}
+
+        # smooth
+        smooth(M, words, n)
 
         # counts to probabilities
         total_count = float(sum(M.values()))
@@ -49,20 +97,48 @@ def model(words, n):
         for w in ngrams(words, n, pad_right=True, pad_left=True):
             M[w[: n-1]][w[-1]] += 1
 
+        # smooth
+        smooth(M, words, n)
+
         # counts to probabilities
-        for w1_w2 in M:
-            total_count = float(sum(M[w1_w2].values()))
-            for w3 in M[w1_w2]:
-                M[w1_w2][w3] /= total_count
+        for wi in M:
+            total_count = float(sum(M[wi].values()))
+            for wj in M[wi]:
+                M[wi][wj] /= total_count
+            M[wi][None] /= M[None]
 
     return M
 
 
-def build_models(words):
-    models = []
+def build_models(X):
+    Ms = []
     for n in range(1, 6):
-        models.append(model(words, n))
+        Ms.append(model(X, n))
+    return Ms
 
 
-words = read_file('sample0.txt')
-models = build_models(words)
+def test_models(Ms, Y):
+    PPs = [PPs.append(Ms[0], Y, unigram=True)]
+    for M in Ms[1:]:
+        PPs.append(pp(M, Y))
+    return PPs
+
+
+def p(model, w1, w2=None):
+    n = len(w1)  # size of model.
+    if n == 1:  # unigram
+        return model[w1]
+    else:
+        return model[w1][w2]
+
+
+def pp(M, Y, unigram=False):
+    x = 0
+    N = len(Y)
+    for i in range(N):
+        x *= 1/p(M, Y[i], Y[i-1] if not unigram else None)
+    return x**(1/N)
+
+
+X, Y = split(read_file('sample0.txt'))
+Ms = build_models(X)
